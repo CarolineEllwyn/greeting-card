6deb45d5c7b50b5bafe4c3d5eb365a09870ac9d9
cellwy <caroline.ellwyn@gmail.com>
2020-11-03 16:50:56 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function messageFunction(){
+    var message = document.getElementById("textAreaId").value;
+    document.getElementById("printedMessage").innerHTML = message;
+    console.log(message)
+}
+
+
+
+  function changeBackgroundColor(){
+      var dropbox = document.getElementById("comboBackground");
+        var selectedColor = dropbox.options[dropbox.selectedIndex].value;
+        if (selectedColor === "1") {
+            document.getElementById("finalForm").style.backgroundColor = "lightblue";
+        }
+        else if (selectedColor === "2") {
+            document.getElementById("finalForm").style.backgroundColor = "red";
+        }
+        else if (selectedColor === "3") {
+            document.getElementById("finalForm").style.backgroundColor = "grey";
+        }
+        else if (selectedColor === "") {
+            document.getElementById("finalForm").style.backgroundColor = "grey";
+        }
+        console.log(selectedColor)
+  }
