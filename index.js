30214c034106af6c16a653cbc0535956d9b67b0b
cellwy <caroline.ellwyn@gmail.com>
2020-11-04 09:00:09 +0100

    formatting text

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 
-function messageFunction(){
+function messageFunction() {
     var message = document.getElementById("textAreaId").value;
     document.getElementById("printedMessage").innerHTML = message;
     console.log(message)
 }
 
-
-
-  function changeBackgroundColor(){
-      var dropbox = document.getElementById("comboBackground");
-        var selectedColor = dropbox.options[dropbox.selectedIndex].value;
-        if (selectedColor === "1") {
-            document.getElementById("finalForm").style.backgroundColor = "lightblue";
-        }
-        else if (selectedColor === "2") {
-            document.getElementById("finalForm").style.backgroundColor = "red";
-        }
-        else if (selectedColor === "3") {
-            document.getElementById("finalForm").style.backgroundColor = "grey";
-        }
-        else if (selectedColor === "") {
-            document.getElementById("finalForm").style.backgroundColor = "grey";
-        }
-        console.log(selectedColor)
-  }
+function changeBackgroundColor() {
+    var dropbox = document.getElementById("comboBackground");
+    var selectedColor = dropbox.options[dropbox.selectedIndex].value;
+    if (selectedColor === "1") {
+        document.getElementById("finalForm").style.backgroundColor = "lightblue";
+    }
+    else if (selectedColor === "2") {
+        document.getElementById("finalForm").style.backgroundColor = "red";
+    }
+    else if (selectedColor === "3") {
+        document.getElementById("finalForm").style.backgroundColor = "grey";
+    }
+    else if (selectedColor === "") {
+        document.getElementById("finalForm").style.backgroundColor = "grey";
+    }
+    console.log(selectedColor)
+}
